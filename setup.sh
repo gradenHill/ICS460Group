dfd117cd3ac510d0fd0ea58f72b989393aa87c62
gradenHill <59981809+gradenHill@users.noreply.github.com>
2026-04-11 17:59:18 -0500

    setup

--- setup.sh
+++ setup.sh
@@ -25,22 +25,22 @@ sudo ip netns add attacker
 sudo ip netns add target
 
 # create the virtual ethernet cable with two named ends
-sudo ip link add veth-attack type veth peer name veth-target
+sudo ip link add veth-attack type veth peer name veth-targetget
 
 # virtually plug the cable into the namespaces
 sudo ip link set veth-attack netns attacker
-sudo ip link set veth-target netns target
+sudo ip link set veth-targetget netns target
 
 # Assign the namespace ip addresses, and tell them to access the network through the cable
 sudo ip netns exec attacker ip addr add 10.0.0.20/24 dev veth-attack
-sudo ip netns exec target ip addr add 10.0.0.10/24 dev veth-target
+sudo ip netns exec target ip addr add 10.0.0.10/24 dev veth-targetget
 
 # Go in the attacker namespace and set the links to "on"
 sudo ip netns exec attacker ip link set veth-attack up
 sudo ip netns exec attacker ip link set lo up
 
 # Go in the target namespace and set the links to "on"
-sudo ip netns exec target ip link set veth-target up
+sudo ip netns exec target ip link set veth-targetget up
 sudo ip netns exec target ip link set lo up
 
 # kill any existing tmux NIDS sessions and start new
@@ -57,7 +57,7 @@ tmux split-window -v -t NIDS:0.0
 # C-m is "enter"
 # -t = allows you to target the tmux pane
 tmux send-keys -t NIDS:0.1 "sudo ip netns exec target bash" C-m
-tmux send-keys -t NIDS:0.1 " history -s 'snort -A console -q -c ./snort.conf -i veth-target -k none'; clear && printf '=== TARGET SPACE (10.0.0.10) ===\\n\\n' && printf 'Press \\033[1;33mUP ARROW\\033[0m to load command, or type it manually:\\n' && printf '\\033[1;32msnort -A console -q -c ./snort.conf -i veth-target -k none\\033[0m\\n' && echo" C-m
+tmux send-keys -t NIDS:0.1 " history -s 'tcpdump -i veth-target -w capture.pcap'; history -s 'snort -A console -q -c ./snort.conf -i veth-target -k none'; clear && printf '=== TARGET SPACE (10.0.0.10) ===\\n\\n' && printf 'HINT: Press \\033[1;33mUP ARROW\\033[0m to cycle through commands:\\n\\n' && printf '1. Start Snort:\\n\\033[1;32msnort -A console -q -c ./snort.conf -i veth-target -k none\\033[0m\\n\\n' && printf '2. Capture PCAP:\\n\\033[1;32mtcpdump -i veth-target -w capture.pcap\\033[0m\\n' && echo" C-m
 
 # Split the window into two screens
 tmux split-window -h -t NIDS:0.1
