fcb5857385b575f95ceadc46cbe10d9a91349258
gradenHill <59981809+gradenHill@users.noreply.github.com>
2026-04-11 17:27:16 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -25,22 +25,22 @@ sudo ip netns add attacker
 sudo ip netns add target
 
 # create the virtual ethernet cable with two named ends
-sudo ip link add virtualEthernetAttackerEnd type veth peer name virtualEthernetTargetEnd
+sudo ip link add veth-attack type veth peer name veth-target
 
 # virtually plug the cable into the namespaces
-sudo ip link set virtualEthernetAttackerEnd netns attacker
-sudo ip link set virtualEthernetTargetEnd netns target
+sudo ip link set veth-attack netns attacker
+sudo ip link set veth-target netns target
 
 # Assign the namespace ip addresses, and tell them to access the network through the cable
-sudo ip netns exec attacker ip addr add 10.0.0.20/24 dev virtualEthernetAttackerEnd
-sudo ip netns exec target ip addr add 10.0.0.10/24 dev virtualEthernetTargetEnd
+sudo ip netns exec attacker ip addr add 10.0.0.20/24 dev veth-attack
+sudo ip netns exec target ip addr add 10.0.0.10/24 dev veth-target
 
 # Go in the attacker namespace and set the links to "on"
-sudo ip netns exec attacker ip link set virtualEthernetAttackerEnd up
+sudo ip netns exec attacker ip link set veth-attack up
 sudo ip netns exec attacker ip link set lo up
 
 # Go in the target namespace and set the links to "on"
-sudo ip netns exec target ip link set virtualEthernetTargetEnd up
+sudo ip netns exec target ip link set veth-target up
 sudo ip netns exec target ip link set lo up
 
 # kill any existing tmux NIDS sessions
@@ -53,7 +53,7 @@ tmux new-session -d -s NIDS
 # C-m is "enter"
 # -t = allows you to target the tmux pane
 tmux send-keys -t NIDS:0.0 "sudo ip netns exec target bash" C-m
-tmux send-keys -t NIDS:0.0 " history -s 'snort -A console -q -c ./snort.conf -i virtualEthernetTargetEnd -k none'; clear && printf '=== TARGET SPACE (10.0.0.10) ===\\n\\n' && printf 'Press \\033[1;33mUP ARROW\\033[0m to load command, or type it manually:\\n' && printf '\\033[1;32msnort -A console -q -c ./snort.conf -i virtualEthernetTargetEnd -k none\\033[0m\\n\\n'" C-m
+tmux send-keys -t NIDS:0.0 " history -s 'snort -A console -q -c ./snort.conf -i veth-target -k none'; clear && printf '=== TARGET SPACE (10.0.0.10) ===\\n\\n' && printf 'Press \\033[1;33mUP ARROW\\033[0m to load command, or type it manually:\\n' && printf '\\033[1;32msnort -A console -q -c ./snort.conf -i veth-target -k none\\033[0m\\n\\n'" C-m
 
 # Split the window into two screens
 tmux split-window -h -t NIDS:0.0
