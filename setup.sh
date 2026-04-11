481db0bd6a8039bdee1dc0734bc950fe6403aac0
gradenHill <59981809+gradenHill@users.noreply.github.com>
2026-04-11 17:11:51 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,14 +53,14 @@ tmux new-session -d -s NIDS
 # C-m is "enter"
 # -t = allows you to target the tmux pane
 tmux send-keys -t NIDS:0.0 "sudo ip netns exec target bash" C-m
-tmux send-keys -t NIDS:0.0 "clear && echo '=== TARGET SPACE (10.0.0.10) ===' && echo '' && echo -e 'COMMAND: \\033[1;32msnort -A console -q -c ./snort.conf -i virtualEthernetTargetEnd -k none\\033[0m'" C-m
+tmux send-keys -t NIDS:0.0 "clear && printf '=== TARGET SPACE (10.0.0.10) ===\n\n' && printf 'Start snort with this command: \033[1;32msnort -A console -q -c ./snort.conf -i virtualEthernetTargetEnd -k none\033[0m\n'" C-m
 
 # Split the window into two screens
 tmux split-window -h -t NIDS:0.0
 
 # RIGHT PANE: Run command to enter the attacker namespace, and display available scripts
 tmux send-keys -t NIDS:0.1 "sudo ip netns exec attacker bash" C-m
-tmux send-keys -t NIDS:0.1 "cd attack-scripts/ && clear && echo '=== ATTACKER SPACE (10.0.0.20) ===' && echo 'Available scripts:' && ls -F && echo '' && echo -e 'USAGE: \\033[1;32mpython3 <scriptName>.py 10.0.0.10\\033[0m'" C-m
+tmux send-keys -t NIDS:0.1 "cd attack-scripts/ && clear && printf '=== ATTACKER SPACE (10.0.0.20) ===\n\n' && ls -F && printf '\nRun an attack script with the following command: \033[1;32mpython3 <scriptName>.py 10.0.0.10\033[0m\n'" C-m
 
 # Enable scrolling and clicking
 tmux set -g mouse on
