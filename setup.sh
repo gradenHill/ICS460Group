e7c8fdb20688462bbf42b4e5b273459cd4c64714
gradenHill <59981809+gradenHill@users.noreply.github.com>
2026-04-11 17:42:55 -0500

    setup.sh changes

--- setup.sh
+++ setup.sh
@@ -48,23 +48,23 @@ tmux kill-session -t NIDS 2>/dev/null
 tmux new-session -d -s NIDS
     
 # TOP PANE: host space
-tmux send-keys -t NIDS:0 "clear && printf '=== MANAGEMENT PANE ===\\n\\n' && printf 'To reset the lab, run:\\n\\033[1;31msudo ./setup.sh\\033[0m\\n' && echo" C-m
+tmux send-keys -t NIDS:0.0 "clear && printf '=== MANAGEMENT PANE ===\\n\\n' && printf 'To reset the lab, run:\\n\\033[1;31msudo ./setup.sh\\033[0m\\n' && echo" C-m
 
 # create bottom section
-tmux split-window -v -t NIDS:0
+tmux split-window -v -t NIDS:0.0
 
 # LEFT PANE: Run command to enter the target namespace
 # C-m is "enter"
 # -t = allows you to target the tmux pane
-tmux send-keys -t NIDS:1 "sudo ip netns exec target bash" C-m
-tmux send-keys -t NIDS:1 " history -s 'snort -A console -q -c ./snort.conf -i veth-target -k none'; clear && printf '=== TARGET SPACE (10.0.0.10) ===\\n\\n' && printf 'Press \\033[1;33mUP ARROW\\033[0m to load command, or type it manually:\\n' && printf '\\033[1;32msnort -A console -q -c ./snort.conf -i veth-target -k none\\033[0m\\n' && echo" C-m
+tmux send-keys -t NIDS:0.1 "sudo ip netns exec target bash" C-m
+tmux send-keys -t NIDS:0.1 " history -s 'snort -A console -q -c ./snort.conf -i veth-target -k none'; clear && printf '=== TARGET SPACE (10.0.0.10) ===\\n\\n' && printf 'Press \\033[1;33mUP ARROW\\033[0m to load command, or type it manually:\\n' && printf '\\033[1;32msnort -A console -q -c ./snort.conf -i veth-target -k none\\033[0m\\n' && echo" C-m
 
 # Split the window into two screens
-tmux split-window -h -t NIDS:1
+tmux split-window -h -t NIDS:0.1
 
 # RIGHT PANE: Run command to enter the attacker namespace, and display available scripts
-tmux send-keys -t NIDS:2 "sudo ip netns exec attacker bash" C-m
-tmux send-keys -t NIDS:2 "cd attack-scripts/ && clear && printf '=== ATTACKER SPACE (10.0.0.20) ===\\n\\nAvailable attack scripts:\\n' && ls -F && printf '\\nRun \\033[1;32mpython3 <scriptName>.py 10.0.0.10\\033[0m to execute attack script\\n' && echo" C-m
+tmux send-keys -t NIDS:0.2 "sudo ip netns exec attacker bash" C-m
+tmux send-keys -t NIDS:0.2 "cd attack-scripts/ && clear && printf '=== ATTACKER SPACE (10.0.0.20) ===\\n\\nAvailable attack scripts:\\n' && ls -F && printf '\\nRun \\033[1;32mpython3 <scriptName>.py 10.0.0.10\\033[0m to execute attack script\\n' && echo" C-m
 
 # Enable scrolling and clicking
 tmux set -g mouse on
