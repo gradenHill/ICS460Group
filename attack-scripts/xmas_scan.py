393ed972f341c0932399c638ded6c05952b03901
vboxuser <vboxuser@Ubuntu-Server.myguest.virtualbox.org>
2026-04-17 16:00:35 -0500

    added xmas scan and made port_scan return open ports

---
+++ attack-scripts/xmas_scan.py
@@ -0,0 +1,21 @@
+import sys
+import random
+from scapy.all import IP, TCP, send
+
+target_ip ="10.0.0.3"
+
+def main():
+    print(f"Starting xmas scan against {target_ip}")
+    ports = [21,22,23,25,53,80,110,139,443]
+    for port in ports:
+        pkt = IP(dst=target_ip) / TCP (
+            sport=random.randint(1024, 65535),
+            dport=port,
+        flags="FPU",
+        seq=random.randint(1000,99999),
+        window=1234
+    )
+        send(pkt,verbose=False)
+        print(f"[XMAS] Sent packet to port {port}")
+if __name__ == "__main__":
+    main()
